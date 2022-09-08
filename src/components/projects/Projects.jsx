1367ec7040572c9508c97611b3bed60ec77ac927
baysaa006 <jargalgalsan@yahoo.com>
2022-09-08 07:08:37 +0200

    demo

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -77,7 +77,7 @@ function Projects() {
               </p>
               <a
                 target="_blank"
-                href="https://card1-nine.vercel.app/signup"
+                href="https://card1-nine.vercel.app/"
                 className="bg-indigo-500 p-2 text-black rounded-xl mt-2"
                 rel="noreferrer"
               >
